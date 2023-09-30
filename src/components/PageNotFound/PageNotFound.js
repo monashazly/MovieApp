bb5e1dba9663ff0f9085c23df4d01ca6e81e634c
Mona Shazly <monashazly@Monas-MacBook-Air.local>
2023-09-30 23:34:14 +0300

    page not found

--- src/components/PageNotFound/PageNotFound.js
+++ src/components/PageNotFound/PageNotFound.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import "./PageNotFound.scss";
 
 const PageNotFound = () => {
     return (
-        <div>
-            page not fiund
+        <div className='page-not-found'>
+            <h2>Oops! We can't find the page you're looking for</h2>
+            <h4>You tried to request a page that doesn't exist</h4>
             
         </div>
     );
