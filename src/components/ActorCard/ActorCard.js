6744c929dc1428c4cab357a11bdeaa214a29a2c1
Mona Shazly <monashazly@Monas-MacBook-Air.local>
2023-12-11 20:38:07 +0200

    add actor image placeholder

--- src/components/ActorCard/ActorCard.js
+++ src/components/ActorCard/ActorCard.js
@@ -1,12 +1,15 @@
 import './ActorCard.scss'
 import { imgBaseURL } from "../../common/apis/imgBaseURL";
+import { IoIosPerson } from "react-icons/io";
 
 const ActorCard = (props) => {
     const { actor } = props 
 
     return (
         <div className='actor-card'>
-            <img className='actor-profile' src={ imgBaseURL + actor.profile_path } alt={ actor.name} />
+            <div className='img-container'>
+               {actor.profile_path ? <img className='actor-profile' src={ imgBaseURL + actor.profile_path } alt={ actor.name} /> : <IoIosPerson color="#b5b5b5" size={50} className="m-auto" />}
+            </div>
             <div className='actor-name'> { actor.name } </div>
             <div className='actor-character'> { actor.character} </div>
         </div>
